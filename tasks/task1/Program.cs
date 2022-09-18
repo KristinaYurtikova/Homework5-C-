d27b7f284f16b93502002c737b3688e51779dfc2
Kristina <kristisha_m@mail.ru>
2022-09-18 22:42:48 +0300

    update task1

--- tasks/task1/Program.cs
+++ tasks/task1/Program.cs
@@ -1,54 +1,25 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-//const int Size = 10;
+Console.WriteLine("Введите количество элементов в массиве: ");
+int count = int.Parse(Console.ReadLine()!);
 
-//int[] array = CreateArray(Size);
-//Console.WriteLine();
-//int search = int.Parse(Console.ReadLine());
+int[] array = new int [count];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,999);
+}
+Console.WriteLine("$ [{string.Join ("," array)}]");
 
-//int [] CreateArray (int [Size]);
-//{
- //   int[] numbers = new int [Size];
- //   for (int i = 0; i < numbers.Length; i++)
- //   {
- //       numbers[i] = new Random().Next(100,999);
- //   }
-  ///  return numbers;
-//}
-
-
-
-
-using System;
-
-    class Program
+int CountEven = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array [i] % 2 == 0)
     {
-        static void Main()
-        {
-            Random myRnd = new Random();// Объявляется генератор случайных чисел.
-            int[] a = new int[10];//Объявление целочисленного массива длинной 10.
-            Console.WriteLine("Исходный массив:");
-
-
-            for (int i = 0; i < a.Length; i++)//Цикл заполнения массива.
-            {
-                a[i] = myRnd.Next(1, 10);//Присвоение случайного числа от 1 до 10 [i]-тому элементу массива.
-                Console.Write(a[i] + " ");//Вывод на экран.
-            }
-            Console.WriteLine("\nРезультат:");
-
-
-            for (int i = 0; i < a.Length; i++)//Цикл вывода массива.
-            {
-                if (a[i] % 2 != 0) a[i] = a[i] * 3;//Если элемент массива нечётный, то умножаем его на 3.
-                else a[i] = 0;//Иначе присваиваем значение элементу 0.             
-                Console.Write(a[i] + " ");
-            }
-            Console.ReadKey();//Задержка консоли.
-        }
+        CountEven++;
     }
-Детали на https://studlearn.com/works/details/chyotnye-i-nechyotnye-elementy-massiva-c-867
+}
+Console.WriteLine($"Количество четных элементов в массиве {CountEven}");
 
 
 

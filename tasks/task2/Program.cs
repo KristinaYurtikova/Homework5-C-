97e0122d110be4233e877d0989e2fc3c23d798c2
Kristina <kristisha_m@mail.ru>
2022-09-19 22:54:16 +0300

    update task2

--- tasks/task2/Program.cs
+++ tasks/task2/Program.cs
@@ -5,34 +5,34 @@
 
 const int Size = 10;
 
-int[] numbers = CreateRandomArray (Size);
-PrintArray (numbers);
-int Sum = CalcSum (numbers);
+int[] numbers = CreateRandomArray(Size);
+PrintArray(numbers);
+int Sum = CalcSum(numbers);
 Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна {Sum}");
 
-void PrintArray (int[] numbers)
+void PrintArray(int[] numbers)
 {
     foreach (int number in numbers)
-    System.Console.Write($"[{string.Join (",", number)}] ");
+        System.Console.Write($"[{string.Join(",", number)}] ");
 }
 
-int [] CreateRandomArray (int Size)
+int[] CreateRandomArray(int Size)
 {
     int[] numbers = new int[Size];
-    for (int i=0; i < Size; i++)
+    for (int i = 0; i < Size; i++)
     {
-        numbers[i] = new Random ().Next (-100, 100);
+        numbers[i] = new Random().Next(-100, 100);
     }
     return numbers;
 }
 
-int CalcSum (int[] numbers)
+int CalcSum(int[] numbers)
 {
     int Sum = 0;
     foreach (int number in numbers)
     {
-        if (number % 2 !=0)
+        if (number % 2 != 0)
             Sum += number;
     }
-    return Sum;   
+    return Sum;
 }
